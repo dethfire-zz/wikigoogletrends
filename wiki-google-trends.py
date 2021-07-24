25c32bc62cd9de36593d20883116b8fef981aa90
Greg Bernhardt <gregbernhardt@gmail.com>
2021-07-24 10:00:33 -0500

    Add files via upload

---
+++ wiki-google-trends.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import requests
+import json
+from pytrends.request import TrendReq
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:50px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<p class="big-font">Wiki and Google Trends</p>
+<p>Compare Wiki Search and Google Search Trends</p>
+<b>Directions: </b></ br><ol>
+<li>Try to be concise and specific. Wiki doesn't handle disambiguation and variations well</li>
+</ol>
+""", unsafe_allow_html=True)
+
+with st.form("data"):
+    query = st.text_input('Enter your keyword','ie - Olympic Games')
+    submitted = st.form_submit_button("Process")
+
+    if submitted:
+        query1 = query
+        query = query.replace(" ","%20")
+
+        url = f'https://en.wikipedia.org/w/api.php?action=query&prop=pageviews&titles={query}&format=json'
+        payload={}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        getjson = json.loads(response.text)
+
+        df = pd.DataFrame(columns = ['Date', 'Hits'])
+
+        for k,item in getjson['query']['pages'].items():
+          for k,item in item['pageviews'].items():
+            data = {'Date': k, 'Hits': item}
+            df = df.append(data, ignore_index=True)
+            
+        df.plot(kind='line', x='Date', y='Hits')
+        
+        
+        st.title("Wiki Pageviews 3 Month")
+        plt.xticks(rotation = 15)
+        st.pyplot()
+        #df.plot.line(x='Date',y='Hits',ylabel='Views',figsize=(10,5),title='Wiki PageView Trend for Python')
+
+
+        pytrends = TrendReq(hl='en-US', tz=360)
+        kw_list = [query1]
+        pytrends.build_payload(kw_list, cat=0, timeframe='today 12-m', geo='', gprop='')
+        df2 = pytrends.interest_over_time()
+        
+        df2.plot(kind='line')
+        
+        st.title("Google Trend 12 Month")
+        plt.xticks(rotation = 15)
+        st.pyplot()
+        #df2.plot.line(ylabel='Views',figsize=(10,5),title='Google Trends for Python')
+st.write('Author: [Greg Bernhardt](https://twitter.com/GregBernhardt4) | Friends: [Rocket Clicks](https://www.rocketclicks.com), [importSEM](https://importsem.com) and [Physics Forums](https://www.physicsforums.com)')
+
